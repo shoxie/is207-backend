798c67fdc25215a83351a69ed58cafa2c8f22fa9
Dangeagle <tenanhladang1506@gmail.com>
2021-09-16 08:29:19 +0700

    cors update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
+var cors = require('cors')
 var app = express();
 const mongoose = require("mongoose");
 mongoose
@@ -23,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors())
 var usersRouter = require("./routes/users.route");
 app.use("/api/auth", usersRouter);
 
